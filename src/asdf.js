b3cff73e36e8dba2fc44a26aa2484f312292b230
DonDejvo <dada542cz@gmail.com>
2021-09-28 22:49:33 +0200

    asdsa

--- src/asdf.js
+++ src/asdf.js
@@ -8,10 +8,10 @@ let __export = {
 if (typeof module === 'object' && typeof module.exports === 'object') {
     module.exports = __export;
 } else if (typeof define === 'function' && define.amd) {
-    define('RayBurst', [], __export);
+    define('asdf', [], __export);
 } else {
     let global = typeof globalThis !== 'undefined' ? globalThis : self;
-    global['RayBurst'] == __export;
+    global['asdf'] = __export;
 }
 
 export default __export;
