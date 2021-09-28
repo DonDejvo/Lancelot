65f2d852e06219e069fcbdff655f607d646ce19a
DonDejvo <dada542cz@gmail.com>
2021-09-28 22:50:09 +0200

    asdsa

--- dist/asdf-cdn-module.js
+++ dist/asdf-cdn-module.js
@@ -10,10 +10,10 @@ var __export = {
 if (typeof module === "object" && typeof module.exports === "object") {
   module.exports = __export;
 } else if (typeof define === "function" && define.amd) {
-  define("RayBurst", [], __export);
+  define("asdf", [], __export);
 } else {
   let global = typeof globalThis !== "undefined" ? globalThis : self;
-  global["RayBurst"] == __export;
+  global["asdf"] = __export;
 }
 var asdf_default = __export;
 export {
