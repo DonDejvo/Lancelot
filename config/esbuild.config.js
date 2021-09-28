2d88bc3b8131b4455c0617633ded850afb7f051f
David <80768788+DonDejvo@users.noreply.github.com>
2021-09-28 22:10:04 +0200

    Update esbuild.config.js

--- config/esbuild.config.js
+++ config/esbuild.config.js
@@ -2,9 +2,17 @@ const esbuild = require('esbuild');
 
 
 esbuild.build({
-    entryPoints: ['src/core.js'],
+    entryPoints: ['src/asdf.js'],
     bundle: true,
     minify: true,
     format: 'esm',
-    outfile: 'dist/lancelot-cdn.min.js'
+    outfile: 'dist/lancelot-cdn-module.min.js'
+});
+
+esbuild.build({
+    entryPoints: ['src/asdf.js'],
+    bundle: true,
+    minify: true,
+    format: 'iife',
+    outfile: 'dist/lancelot-cdn-nomodule.min.js'
 });
