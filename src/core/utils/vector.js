2e50e7dc1d5af331300c3cb5332533e0f923c08b
DonDejvo <dada542cz@gmail.com>
2021-09-29 22:28:04 +0200

    initial commit

--- src/core/utils/vector.js
+++ src/core/utils/vector.js
@@ -16,29 +16,29 @@ export class Vector {
         this._y = num;
     }
     Copy(v1) {
-        this._x = v1.x;
-        this._y = v1.y;
+        this._x = v1._x;
+        this._y = v1._y;
     }
     Clone() {
-        return new Vector(this.x, this.y);
+        return new Vector(this._x, this._y);
     }
     Add(v1) {
-        this.x += v1.x;
-        this.y += v1.y;
+        this._x += v1._x;
+        this._y += v1._y;
         return this;
     }
     Sub(v1) {
-        this.x -= v1.x;
-        this.y -= v1.y;
+        this._x -= v1._x;
+        this._y -= v1._y;
         return this;
     }
     Mult(s) {
-        this.x *= s;
-        this.y *= s;
+        this._x *= s;
+        this._y *= s;
         return this;
     }
     Norm() {
-        [this.x, this.y] = [this.y, -this.x];
+        [this._x, this._y] = [this._y, -this._x];
         return this;
     }
     Unit() {
@@ -46,32 +46,32 @@ export class Vector {
         if (z === 0) {
             return this;
         }
-        this.x /= z;
-        this.y /= z;
+        this._x /= z;
+        this._y /= z;
         return this;
     }
     Mag() {
-        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+        return Math.sqrt(Math.pow(this._x, 2) + Math.pow(this._y, 2));
     }
     Lerp(v1, alpha) {
         this.Add(v1.Clone().Sub(this).Mult(alpha));
         return this;
     }
     Angle() {
-        return Math.atan2(this.y, this.x);
+        return Math.atan2(this._y, this._x);
     }
     Rotate(angle) {
-        const x = this.x * Math.cos(angle) - this.y * Math.sin(angle);
-        const y = this.x * Math.sin(angle) + this.y * Math.cos(angle);
-        this.x = x;
-        this.y = y;
+        const x = this._x * Math.cos(angle) - this._y * Math.sin(angle);
+        const y = this._x * Math.sin(angle) + this._y * Math.cos(angle);
+        this._x = x;
+        this._y = y;
         return this;
     }
     static Dot(v1, v2) {
-        return v1.x * v2.x + v1.y * v2.y;
+        return v1._x * v2._x + v1._y * v2._y;
     }
     static Dist(v1, v2) {
-        return Math.sqrt(Math.pow((v1.x - v2.x), 2) + Math.pow((v1.y - v2.y), 2));
+        return Math.sqrt(Math.pow((v1._x - v2._x), 2) + Math.pow((v1._y - v2._y), 2));
     }
     static AngleBetween(v1, v2) {
         const z1 = v1.Mag();
