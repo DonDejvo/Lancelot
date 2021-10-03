3a55e457bcbfb4a6b0412ad801e9a86f22fd4da2
DonDejvo <dada542cz@gmail.com>
2021-10-03 12:16:34 +0200

    initial commit

--- web-tests/audio/main.js
+++ web-tests/audio/main.js
@@ -1,4 +1,4 @@
-import defaultExport from "https://cdn.jsdelivr.net/gh/DonDejvo/Lancelot/dist/lancelot-cdn-module.min.js";
+import defaultExport from "https://cdn.jsdelivr.net/gh/DonDejvo/Lancelot/dist/lancelot-cdn-module.js";
 
         const game = new Lancelot.Game({
             width: 640,
